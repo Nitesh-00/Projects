fcee9443550731ab9cec48180544df1e67424ad7
Nitesh-00 <niteshsingla00@gmail.com>
2025-06-23 20:28:14 +0530

    Create NavBar

---
+++ E-Commerce/frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes,Route } from "react-router-dom";
+import Home from './pages/Home'
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Orders from "./pages/Orders";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import PlaceOrder from "./pages/PlaceOrder";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/collection" element={<Collection/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/product/:productId" element={<Product/>}/>
+          <Route path="/Cart" element={<Cart/>}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/place-order" element={<PlaceOrder/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
